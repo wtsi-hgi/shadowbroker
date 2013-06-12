b4b952599d088e3f7d6d8996b0d6487a93f94995
Nicholas Clarke <nc6@sanger.ac.uk>
2013-06-12 09:50:21 +0100

    Actually do what we want in terms of routing.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,19 +1,31 @@
 package uk.ac.sanger.hgi.shadowbroker
 
+import org.apache.camel.CamelContext
 import org.apache.camel.impl.DefaultCamelContext
-import org.apache.camel.scala.dsl.builder.RouteBuilder
+import org.apache.camel.component.xmpp.XmppEndpoint
+import org.apache.camel.builder.RouteBuilder
 
 object Main {
 
   def main(args : Array[String]) {
     val context = new DefaultCamelContext
-    context.addRoutes(TestRoutes)
+    context.addRoutes(routes(context))
     context.start()
+    while(true) {}
   }
 
-}
+  def routes(context : CamelContext) = {
+    // We share the endpoint here 
+    val xmppEndpoint = context.getEndpoint(
+      "xmpp://nc6@hgi-im.internal.sanger.ac.uk?room=hgi-team&password=",
+      classOf[XmppEndpoint]
+      )
 
-object TestRoutes extends RouteBuilder {
-  "file://test" -->
-  "xmpp://nc6@hgi-im.internal.sanger.ac.uk?room=hgi-team&password="
+    new RouteBuilder {
+      def configure() = {
+        from("imaps://nc6@imapproxy.sanger.ac.uk?password=") to xmppEndpoint
+        from(xmppEndpoint) to "file://test"
+      }
+    }
+  }
 }
